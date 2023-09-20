23f8940c9bee2cfef7a4205f15d5830b581fe1b6
Romi <ouzhong085@163.com>
2023-09-19 20:06:25 -0400

    checkin ai

--- automate-ai/src/test/java/ChatTest.java
+++ automate-ai/src/test/java/ChatTest.java
@@ -4,11 +4,11 @@ import com.ssc.tortoise.automate.ai.ChatTortoise;
 public class ChatTest {
     public static void main(String [] args) throws InterruptedException {
 //        ChatMessage msgAns = ChatTortoise.getSingleton().chat("What's the best way to train a parrot?");
-        ChatMessage msgAns = ChatTortoise.getSingleton().chat("wait 3 second");
+        ChatMessage msgAns = ChatTortoise.getSingleton().chat("I want to click the '10548.png'");
         System.out.println("Message: " + msgAns.getContent());
-//        Thread.sleep(5000);
+        Thread.sleep(5000);
 //
-//        msgAns = ChatTortoise.getSingleton().chat("What's a parrot' favorite food?");
-//        System.out.println("Message: " + msgAns.getContent());
+        msgAns = ChatTortoise.getSingleton().chat("open chrome");
+        System.out.println("Message: " + msgAns.getContent());
     }
 }
