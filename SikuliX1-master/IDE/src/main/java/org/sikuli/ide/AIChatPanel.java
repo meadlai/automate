ad6106731bd51e5d5ea9d37978738324b8b88a74
meadlai <laiiqnyi@gmail.com>
2023-09-21 16:44:19 +0800

    get text in the editor

--- SikuliX1-master/IDE/src/main/java/org/sikuli/ide/AIChatPanel.java
+++ SikuliX1-master/IDE/src/main/java/org/sikuli/ide/AIChatPanel.java
@@ -7,19 +7,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.net.URL;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JViewport;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
 
 import org.sikuli.basics.Debug;
-import org.sikuli.basics.Settings;
 
 import com.azure.ai.openai.models.ChatMessage;
 import com.ssc.tortoise.automate.ai.ChatTortoise;
@@ -35,7 +38,7 @@ public class AIChatPanel extends JSplitPane implements Runnable {
 	private JButton btnAccept = new JButton("Accept");
 	private GridBagLayout layout = new GridBagLayout();
 	private GridBagConstraints c = new GridBagConstraints();
-	
+
 	public static String newline = System.getProperty("line.separator");
 
 	public AIChatPanel() {
@@ -65,7 +68,7 @@ public class AIChatPanel extends JSplitPane implements Runnable {
 			}
 
 		});
-		
+
 		chatHistory.addFocusListener(new FocusListener() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -86,12 +89,47 @@ public class AIChatPanel extends JSplitPane implements Runnable {
 		});
 
 	}
-	
+
 	JSplitPane buildChatComponent() {
 		chatPanel.add(btnSend);
 		chatPanel.add(btnAccept);
 		return new JSplitPane(JSplitPane.VERTICAL_SPLIT, chatInputBox, chatPanel);
-		
+
+	}
+	
+	public String getChatInputString() {
+		Document doc = this.chatInputBox.getDocument();
+		int count = doc.getDefaultRootElement().getElementCount();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<count ; i++) {
+			Element item = doc.getDefaultRootElement().getElement(i);
+			String result = getLineTextFrom(item);
+			sb.append(result);
+		}
+		return sb.toString();
+	}
+	
+	private String getLineTextFrom(Element elem) {
+		Document doc = elem.getDocument();
+		Element subElem;
+		String text;
+		String line = "";
+		for (int i = 0; i < elem.getElementCount(); i++) {
+			text = "";
+			subElem = elem.getElement(i);
+			int start = subElem.getStartOffset();
+			int end = subElem.getEndOffset();
+			if (subElem.getName().contains("component")) {
+				text = StyleConstants.getComponent(subElem.getAttributes()).toString();
+			} else {
+				try {
+					text = doc.getText(start, end - start);
+				} catch (Exception ex) {
+				}
+			}
+			line += text;
+		}
+		return line.trim();
 	}
 
 	public static EditorPane getActiveEditPanel() {
@@ -127,56 +165,56 @@ public class AIChatPanel extends JSplitPane implements Runnable {
 	private static void fatal(String message, Object... args) {
 		Debug.logx(-1, me + "FATAL: " + message, args);
 	}
-	
+
 	class ButtonChatSend extends JButton implements ActionListener {
 
-	    private Thread thread = null;
+		private Thread thread = null;
 
-	    ButtonChatSend() {
-	      super();
+		ButtonChatSend() {
+			super();
 
 //	      URL imageURL = SikulixIDE.class.getResource("/icons/run_big_green.png");
 //	      setIcon(new ImageIcon(imageURL));
-	      initTooltip();
-	      addActionListener(this);
-	      setText("Send");
-	      //setMaximumSize(new Dimension(45,45));
-	    }
+			initTooltip();
+			addActionListener(this);
+			setText("Send");
+			// setMaximumSize(new Dimension(45,45));
+		}
 
-	    private void initTooltip() {
+		private void initTooltip() {
 //	      PreferencesUser pref = PreferencesUser.get();
 //	      String strHotkey = Key.convertKeyToText(
 //	          pref.getStopHotkey(), pref.getStopHotkeyModifiers());
 //	      String stopHint = _I("btnRunStopHint", strHotkey);
 //	      setToolTipText(_I("btnRun", stopHint));
-	    }
+		}
 
-	    @Override
-	    public void actionPerformed(ActionEvent ae) {
-	    	runChat();
-	    }
-	    
-	    void runChat() {
+		@Override
+		public void actionPerformed(ActionEvent ae) {
+			runChat();
+		}
+
+		void runChat() {
 //	    	String currentChatMsg = ((EditorPane)chatTabs.getTabComponentAt(0)).getText();
-	    	String currentChatMsg = chatInputBox.getText();
-	    	
-	    	ChatMessage msgAns = ChatTortoise.getSingleton().chat(currentChatMsg);
-	    	String aiRes = "";
-	    	if(msgAns != null && msgAns.getContent() != null) {
-	    		aiRes = msgAns.getContent().replace("<code-line>", "").replace("</code-line>", "");
-	    	}
-	    	String currentChatConversactionMsg = chatHistory.getText();
-	    	StringBuffer strBuffer = new StringBuffer();
-	    	strBuffer.append(currentChatConversactionMsg)
-	    	.append(newline)
-	    	.append(currentChatMsg)
-	    	.append(newline)
-	    	.append(aiRes);
-	    	chatInputBox.setText("");
-	    	chatHistory.setText(strBuffer.toString());
-	    	Debug.log(3, currentChatMsg);
-	    }
+			String currentChatMsg = getChatInputString();
+			//
 
+			error("Input is: " + currentChatMsg);
+			ChatMessage msgAns = ChatTortoise.getSingleton().chat(currentChatMsg);
+			String aiRes = "";
+			error("Output is: " + msgAns.getContent());
+			if (msgAns != null && msgAns.getContent() != null) {
+				aiRes = msgAns.getContent().replace("<code-line>", "").replace("</code-line>", "");
+			}
+
+			String currentChatConversactionMsg = chatHistory.getText();
+			StringBuffer strBuffer = new StringBuffer();
+			strBuffer.append(currentChatConversactionMsg).append(newline).append(currentChatMsg).append(newline)
+					.append(aiRes);
+			chatInputBox.setText("");
+			chatHistory.setText(strBuffer.toString());
+			Debug.log(3, currentChatMsg);
+		}
 
 //	    void doBeforeRun() {
 //	      Settings.ActionLogs = prefs.getPrefMoreLogActions();
